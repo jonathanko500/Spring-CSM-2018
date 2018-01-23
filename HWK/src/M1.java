20ec74bd479268acbd7d1d9dec43b46e03f9aa52
jonathanko500 <jkko9010@gmail.com>
2018-01-22 16:21:39 -0800

    M1 q1-30
    29 more to go

---
+++ HWK/src/M1.java
@@ -0,0 +1,12 @@
+public class M1
+{//start class
+
+	public static void main(String[] args)
+	{//start main
+		boolean done = false;
+		int x = 11, y = 11;
+		boolean expression = (!done && x<=y);
+		System.out.println(expression);
+	}//end main
+	
+}//end class

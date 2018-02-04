2fa00fa49c48ec63d4c2b0a3d3dd0a94c64f9f48
jonathanko500 <jkko9010@gmail.com>
2018-02-03 19:13:18 -0800

    M2 test
    Q 1-31 -(18-22)

---
+++ HWK/src/M2Test.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class M2Test
+{
+	public static void main(String[] args)
+	{//start main
+		String book = "Java is the best.";
+		String result = book.substring(8, 9);
+		System.out.println(result);
+	}//end main
+}

fb8f975de8aedb564311c4620db7b7b705fe1be6
jonathanko500 <jkko9010@gmail.com>
2018-02-04 23:04:58 -0800

    M2 test
    rest of M2 test

--- HWK/src/M2Test.java
+++ HWK/src/M2Test.java
@@ -1,11 +1,6 @@
-import java.util.Random;
 
 public class M2Test
-{
-	public static void main(String[] args)
-	{//start main
-		String book = "Java is the best.";
-		String result = book.substring(8, 9);
-		System.out.println(result);
-	}//end main
-}
+{//start class
+	
+	
+}//end class

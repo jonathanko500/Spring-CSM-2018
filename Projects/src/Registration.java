2687dbbea2f6ef6bb1bdcbba426997eed5896cba
jonathanko500 <jkko9010@gmail.com>
2018-02-17 16:53:06 -0800

    project 2
    started

---
+++ Projects/src/Registration.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+// project 2
+public class Registration
+{//start class
+	public static void main(String[] args)
+	{//start main
+		ArrayList<String> numbers = new ArrayList<>();
+	}//end main
+}//end class

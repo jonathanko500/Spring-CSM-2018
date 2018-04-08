8371d79c31f66fad8ad5c37b234e89ffdfe34199
jonathanko500 <jkko9010@gmail.com>
2018-04-08 15:47:29 -0700

    M10Prac
    Got a pic to open

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4d/f0d46149793b0018180ca571be889c00
@@ -0,0 +1,10 @@
+
+public class M10Prac extends Application{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- M10/src/M10Prac.java
+++ M10/src/M10Prac.java
@@ -1,10 +1,34 @@
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.stage.*;
 
-public class M10Prac extends Application{
-
+public class M10Prac extends Application
+{//start class
+	
+	public void start(Stage primaryStage)
+	{
+		VBox primaryBox = new VBox();
+		
+		Image pic = new Image("dog1.jpg",500,300,true,true);
+		ImageView picView = new ImageView(pic);
+		primaryBox.getChildren().add(picView);
+		
+		Scene scene = new Scene(primaryBox,700,700,Color.WHITE);
+		primaryStage.setTitle("GUI Image");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		
+	}
+	
+	
+	
+	
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
-
-}
+}//end class

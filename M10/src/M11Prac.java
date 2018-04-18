f0d5554a5e9537ba64a2a0c1c506b1a90d4d33e9
jonathanko500 <jkko9010@gmail.com>
2018-04-17 21:02:59 -0700

    M11Test

--- M10/src/M11Prac.java
+++ M10/src/M11Prac.java
@@ -12,14 +12,17 @@ public class M11Prac extends Application
 {
 	private CheckBox description;
 	private Text write;
+	private RadioButton dog1,dog2;
+	private Image first,second;
+	private ImageView picView;
 	public void start(Stage primaryStage)
 	{
 		//pane
 		VBox box = new VBox();
 		box.setAlignment(Pos.CENTER);
 		//picture
-		Image pic = new Image("dog2.jpg",300,300,true,true);
-		ImageView picView = new ImageView(pic);
+		first = new Image("dog2.jpg",300,300,true,true);
+		picView = new ImageView(first);
 		box.getChildren().add(picView);
 		//text
 		write = new Text("My lazy and happy dog.");
@@ -32,6 +35,20 @@ public class M11Prac extends Application
 		description.setSelected(true);
 		description.setOnAction(this::handleDescription);
 		box.getChildren().add(description);
+		//radio button
+		ToggleGroup picGroup= new ToggleGroup();
+		dog1 = new RadioButton("Pic 1");
+		dog1.setToggleGroup(picGroup);
+		dog1.setSelected(true);
+		dog1.setOnAction(this::handleRadio);
+		
+		dog2 = new RadioButton("Pic 2");
+		dog2.setToggleGroup(picGroup);
+		dog2.setOnAction(this::handleRadio);
+		
+		HBox radioButton= new HBox(dog1,dog2);
+		radioButton.setAlignment(Pos.CENTER);
+		box.getChildren().add(radioButton);
 		//display
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("M11 Prac");
@@ -47,6 +64,18 @@ public class M11Prac extends Application
 		else if(!description.isSelected())
 		{
 			write.setVisible(false);
+		}
+	}
+	private void handleRadio(ActionEvent event)
+	{
+		if(dog1.isSelected())
+		{
+			picView.setImage(first);
+		}
+		else if(dog2.isSelected())
+		{
+			second = new Image("dog.jpg",300,300,true,true);
+			picView.setImage(second);
 		}
 	}
 	public static void main(String[] args) {
--- M10/src/M11Test.java
+++ M10/src/M11Test.java
@@ -1,36 +1,99 @@
+import java.util.*;
+
 import javafx.application.*;
+import javafx.event.*;
+import javafx.geometry.Pos;
 import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.*;
 import javafx.scene.shape.*;
+import javafx.scene.text.*;
 import javafx.stage.*;
 
 public class M11Test extends Application
 {//class
+	
 	public void start(Stage primStage)
 	{
-		VBox pane = new VBox();
+		TextInputDialog input = new TextInputDialog();
+		input.setHeaderText("g to oz");
+		input.setTitle("Weight Converter");
+		input.setContentText("Enter grams");
 		
-		Circle circle = new Circle(100, 100, 50);
-
-		circle.setFill(Color.GREEN);
-
-		Rectangle rectangle = new Rectangle(70, 70, 225, 75);
-
-		rectangle.setStroke(Color.BLUE);
-
-		rectangle.setStrokeWidth(2);
-
-		rectangle.setFill(Color.TRANSPARENT);
-
-		pane.getChildren().add(circle);
-
-		pane.getChildren().add(rectangle);
+		Optional<String> userInput=input.showAndWait();
+		if(userInput.isPresent())
+		{
+			String userNumb =userInput.get();
+			int gram = Integer.parseInt(userNumb);
+			double oz=gram*0.035274;
+			Alert resultAlert = new Alert(AlertType.INFORMATION);
+			resultAlert.setTitle("Results");
+			resultAlert.setHeaderText(null);
+			resultAlert.setContentText(gram+" grams is also "+oz+" ounces");
+			resultAlert.showAndWait();
+		}
+		else
+		{
+			System.out.println("no");
+		}
+		/*
+		VBox pane = new VBox();
 		
 		Scene scene = new Scene(pane,400,400,Color.TRANSPARENT);
 		primStage.setScene(scene);
 		primStage.show();
+		*/
 	}
+	/*
+	public void handlePurchaseButton(ActionEvent event)
+	{
+		if(regularShippingSpeedRadioButton.isSelected())
+		{//start if for regular
+			if(item1Checkbox.isSelected())
+			{
+				summaryTextField=new Text("You purchaced these items at regular shipping: "+item1Checkbox);
+			}
+			else if(item1Checkbox.isSelected()&&item2Checkbox.isSelected())
+			{
+				summaryTextField=new Text("You purchaced these items at regular shipping: "+item1Checkbox+" "+item2Checkbox);
+
+			}
+			else if(item1Checkbox.isSelected()&&item2Checkbox.isSelected()&&item3Checkbox.isSelected())
+			{
+				summaryTextField=new Text("You purchaced these items at regular shipping: "+item1Checkbox+" "+item2Checkbox+" "+item3Checkbox);
+
+			}
+			else
+			{
+				summaryTextField=new Text("You purchaced nothing at regular shipping: ");
+			}
+		}//end if for regular
+		else if(fastShippingSpeedRadioButton.isSelected())
+		{//start if for fast
+			if(item1Checkbox.isSelected())
+			{
+				summaryTextField=new Text("You purchaced these items at fast shipping: "+item1Checkbox);
+			}
+			else if(item1Checkbox.isSelected()&&item2Checkbox.isSelected())
+			{
+				summaryTextField=new Text("You purchaced these items at fast shipping: "+item1Checkbox+" "+item2Checkbox);
+
+			}
+			else if(item1Checkbox.isSelected()&&item2Checkbox.isSelected()&&item3Checkbox.isSelected())
+			{
+				summaryTextField=new Text("You purchaced these items at fast shipping: "+item1Checkbox+" "+item2Checkbox+" "+item3Checkbox);
+
+			}
+			else
+			{
+				summaryTextField=new Text("You purchaced nothing at fast shipping: ");
+			}
+		}//end regular for fast
+	}
+	*/
 	public static void main(String[] args) {
 		launch(args);
 

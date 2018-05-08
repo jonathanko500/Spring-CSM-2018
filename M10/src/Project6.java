361aa9304ae326a187a5df11e83ce172e3f102ae
jonathanko500 <jkko9010@gmail.com>
2018-05-07 22:20:39 -0700

    Project 6

---
+++ M10/src/Project6.java
@@ -0,0 +1,14 @@
+import javafx.application.*;
+import javafx.stage.*;
+
+public class Project6 extends Application{
+	public void start(Stage primaryStage)  {
+		
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+}
